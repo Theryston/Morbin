bef01034892d34df61952bf501eccc2f7436c8c3
Theryston <funktodo2@gmail.com>
2021-04-08 13:11:00 -0300

    the types

--- examples/test.js
+++ examples/test.js
@@ -1,18 +1,19 @@
 //require the MorBin
 const MorBin = require('../')
+const morbin = new MorBin('number')
 
 // a text for encode
 var text = `hello world!`
 
 //encoding the text
-var encoded = MorBin.encode(text)
+var encoded = morbin.encode(text)
 
 //logs
 console.log(`the phrase '${text}' in Morbin is:`)
 console.log(encoded)
 
 //decoding the morbin generated
-var decoded = MorBin.decode(encoded)
+var decoded = morbin.decode(encoded)
 
 //logs
 console.log(`and the code decoded above is:`)
